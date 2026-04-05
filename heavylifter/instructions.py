5a11ac9371d39d80144f416b210bfda467c6bc97
oknav <csilla.vanko@outlook.com>
2026-04-05 20:45:03 +0200

    💩 started fixing logic + add validation

--- heavylifter/instructions.py
+++ heavylifter/instructions.py
@@ -8,7 +8,7 @@ class InvalidInputException(Exception):
     pass
 
 
-Stacks = list[list[str]]
+Stacks = dict[int, list[list[str]]]
 
 
 class Movement(TypedDict):
@@ -35,6 +35,19 @@ def _open_file(abs_path: str) -> str:
     return ""
 
 
+def _pre_validate(instructions: str) -> bool:
+    bottoms = re.findall("bottom", instructions)
+    if not len(bottoms) == 1:
+        raise InvalidInputException(
+            "'bottom' must be present in input instructions exactly once"
+        )
+
+    if re.match(pattern=r"\|\|", string=instructions):
+        raise InvalidInputException("No empty boxes are allowed")
+
+    return True
+
+
 ### Validation:
 # bottom is present
 # stack validation:
@@ -50,10 +63,11 @@ def _open_file(abs_path: str) -> str:
 
 class Instruction:
     def __init__(self, instructions: str):
-        self._stacks, self._movements = self._split_instructions(instructions)
+        _pre_validate(instructions)
+        self._stacks, self._movements = self._clean_instructions(instructions)
 
     @staticmethod
-    def _clean_lines(lines: list[str]):
+    def _filter_empty(lines: list[str]):
         return [line for line in lines if line.strip()]
 
     @classmethod
@@ -71,17 +85,52 @@ class Instruction:
 
         instruction_split = re.split(pattern="bottom", string=instructions)
         return (
-            cls._clean_lines(instruction_split[0].splitlines()),
-            cls._clean_lines(instruction_split[1].splitlines()),
+            cls._filter_empty(instruction_split[0].splitlines()),
+            cls._filter_empty(instruction_split[1].splitlines()),
         )
+
+    @staticmethod
+    def _validate_stacks(stacks: list[str]):
+        # last row should contain stack numbers less than 10
+        stack_number_row = stacks[-1].strip()
+
+        # match for exactly 1 number in row or at most 9 numbers with 3 whitespaces and a number at the end
+        if not re.match(
+            pattern=r"^(\d+)$|^(\d+\s{3}){1,9}\d+$",
+            string=stack_number_row,
+        ):
+            raise InvalidInputException(
+                "Format for numbering the stacks might be incorrect (maximum 10 stacks are allowed)"
+            )
+
+        # check if boxes are unique
+        labelled_boxes = re.findall(pattern=r"(\|[A-Z]\|)", string="\n".join(stacks))
+
+        if len(labelled_boxes) != len(set(labelled_boxes)):
+            raise InvalidInputException("Box labels must be globally unique")
+
+    @staticmethod
+    def _validate_movements(movements: list[str]):
+        pass
+
+    def _clean_instructions(self, instructions: str) -> tuple[list[str], list[str]]:
+        stacks_raw, movements_raw = self._split_instructions(instructions)
+        self._validate_stacks(stacks=stacks_raw)
+        self._validate_movements(movements=movements_raw)
+
+        return stacks_raw, movements_raw
+
+        # no empty boxes
 
     @cached_property
     def stacks(self) -> Stacks:
+        # get numbers only from string
+        stack_numbers = map(
+            int, re.findall(pattern=r"\d+", string=self._stacks.pop(-1))
+        )
+
         # pattern to ensure empty spots in stack get parsed as well
         box_pattern = re.compile(r"(\s{3}|\|[A-Z]\|)\s?")
-
-        # pop last stack as it containes the number of stacks
-        self._stacks.pop(-1)
 
         stacks_by_row = [
             re.findall(pattern=box_pattern, string=_stacks) for _stacks in self._stacks
@@ -90,11 +139,11 @@ class Instruction:
             list([box for box in row if box.strip()]) for row in zip(*stacks_by_row)
         ]
 
-        return transposed_stacks
+        return dict(zip(stack_numbers, transposed_stacks))
 
     @cached_property
     def movements(self) -> list[Movement]:
-        match_all_numbers = r"\d"
+        match_all_numbers = r"\d+"
 
         movements = []
         for move in self._movements:
