a1576ec5f0c67820b634eb1275bcd44d4abe41c5
oknav <csilla.vanko@outlook.com>
2026-04-06 16:19:24 +0200

    🎨 add docstrings to robot.pys

--- heavylifter/v1/robot.py
+++ heavylifter/v1/robot.py
@@ -6,8 +6,20 @@ from heavylifter.types import Movement, Robot, Stacks
 class LimitedRobot(Robot):
     @staticmethod
     def lift(stacks: Stacks, movement: Movement) -> Stacks:
+        """Lift 1 box at a time from the front of the source stack at once and place it to the front of the destination stack as many times as the instruction states
+
+        Args:
+            stacks (Stacks): Stacks with IDs containing the boxes
+            movement (Movement): Instruction for the current movement
+
+        Returns:
+            Stacks: Stacks of boxes with IDs after the instruction was carried out
+        """
         result_stacks = copy(stacks)
-        for _ in range(min(movement.box_count, len(result_stacks[movement.src_id]))):
+
+        # if the instruction box count exceeds the actual number of boxes in the source stack, move the available boxes only
+        lift_count = min(movement.box_count, len(result_stacks[movement.src_id]))
+        for _ in range(lift_count):
             result_stacks[movement.dst_id] = [
                 result_stacks[movement.src_id].pop(0)
             ] + result_stacks[movement.dst_id]
--- heavylifter/v2/robot.py
+++ heavylifter/v2/robot.py
@@ -6,6 +6,15 @@ from heavylifter.types import Movement, Robot, Stacks
 class StrongerRobot(Robot):
     @staticmethod
     def lift(stacks: Stacks, movement: Movement) -> Stacks:
+        """Lift given number of boxes at once from the front of the source stack at once and place it to the front of the destination stack
+
+        Args:
+            stacks (Stacks): Stacks with IDs containing the boxes
+            movement (Movement): Instruction for the current movement
+
+        Returns:
+            Stacks: Stacks of boxes with IDs after the instruction was carried out
+        """
         result_stacks = copy(stacks)
         result_stacks[movement.dst_id] = (
             result_stacks[movement.src_id][: movement.box_count]
